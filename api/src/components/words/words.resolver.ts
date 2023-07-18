98cfffff21595f1bb2c792e6a5f0c0b088532387
hiroshi <hiroshi.tan57@gmail.com>
2023-07-18 15:51:30 -0400

    refactor: words module with standards structure

---
+++ api/src/components/words/words.resolver.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { Args, Query, Resolver, Mutation, Context } from '@nestjs/graphql';
+
+import { WordsService } from './words.service';
+
+import {
+  Word,
+  WordReadInput,
+  WordReadOutput,
+  WordUpsertOutput,
+  WordUpsertInput,
+} from './types';
+import { getBearer } from 'src/common/utility';
+
+@Injectable()
+@Resolver(Word)
+export class WordsResolver {
+  constructor(private wordService: WordsService) {}
+
+  @Query(() => WordReadOutput)
+  async wordReadResolver(
+    @Args('input') input: WordReadInput,
+  ): Promise<WordReadOutput> {
+    console.log('word read resolver, word_id:', input.word_id);
+
+    return this.wordService.read(input);
+  }
+
+  @Mutation(() => WordUpsertOutput)
+  async wordUpsertResolver(
+    @Args('input') input: WordUpsertInput,
+    @Context() req: any,
+  ): Promise<WordUpsertOutput> {
+    console.log('word upsert resolver, string: ', input.wordlike_string);
+
+    return this.wordService.upsert(input, getBearer(req));
+  }
+}
---
+++ api/src/components/words/words.service.ts
@@ -0,0 +1,96 @@
+import { Injectable } from '@nestjs/common';
+
+import { ErrorType } from 'src/common/types';
+import { PostgresService } from 'src/core/postgres.service';
+
+import { WordReadInput, WordReadOutput, WordUpsertInput } from './types';
+
+import {
+  getWordObjById,
+  GetWordObjectById,
+  callWordUpsertProcedure,
+  WordUpsertProcedureOutputRow,
+} from './sql-string';
+
+@Injectable()
+export class WordsService {
+  constructor(private pg: PostgresService) {}
+
+  async read(input: WordReadInput): Promise<WordReadOutput> {
+    try {
+      const res1 = await this.pg.pool.query<GetWordObjectById>(
+        ...getWordObjById(input.word_id),
+      );
+
+      if (res1.rowCount !== 1) {
+        console.error(`no word for id: ${input.word_id}`);
+      } else {
+        return {
+          error: ErrorType.NoError,
+          word: {
+            word_id: input.word_id,
+            word: res1.rows[0].word,
+            definition:
+              res1.rows[0].word_definition_id && res1.rows[0].definition
+                ? {
+                    word_definition_id: res1.rows[0].word_definition_id,
+                    definition: res1.rows[0].definition,
+                  }
+                : null,
+            language_code: res1.rows[0].language_code,
+            dialect_code: res1.rows[0].dialect_code,
+            geo_code: res1.rows[0].geo_code,
+          },
+        };
+      }
+    } catch (e) {
+      console.error(e);
+    }
+
+    return {
+      error: ErrorType.UnknownError,
+      word: null,
+    };
+  }
+
+  async upsert(
+    input: WordUpsertInput,
+    token?: string,
+  ): Promise<WordReadOutput> {
+    try {
+      const res = await this.pg.pool.query<WordUpsertProcedureOutputRow>(
+        ...callWordUpsertProcedure({
+          wordlike_string: input.wordlike_string,
+          language_code: input.language_code,
+          dialect_code: input.dialect_code,
+          geo_code: input.geo_code,
+          token: token,
+        }),
+      );
+
+      const creatingError = res.rows[0].p_error_type;
+      const word_id = res.rows[0].p_word_id;
+
+      if (creatingError !== ErrorType.NoError || !word_id) {
+        return {
+          error: creatingError,
+          word: null,
+        };
+      }
+
+      const { error: readingError, word } = await await this.read({ word_id });
+
+      return {
+        error: readingError,
+        word,
+      };
+    } catch (e) {
+      console.error(e);
+    }
+
+    return {
+      error: ErrorType.UnknownError,
+      word: null,
+    };
+  }
+}
