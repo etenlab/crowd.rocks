eb87ec14d86860adfe60ee216cb5290e6b707139
Ihor_S <ihor.shcherbyna@clockwise.software>
2023-07-18 20:51:06 +0200

    fix: add some sub-routing and maps pages structure

---
+++ frontend/src/components/map/MapDetails/MapDetails.tsx
@@ -0,0 +1,11 @@
+import { Caption } from '../../common/Caption/Caption';
+export type TMapDetailsProps = {};
+
+export const MapDetails = () => {
+  return (
+    <>
+      <Caption>Map details here</Caption>
+      <div>and some image</div>
+    </>
+  );
+};
--- frontend/src/components/map/MapList/MapsList.tsx
+++ frontend/src/components/map/MapList/MapsList.tsx
@@ -1,16 +1,7 @@
-import {
-  IonBackButton,
-  IonButton,
-  IonContent,
-  IonHeader,
-  IonIcon,
-  IonItem,
-  IonList,
-  IonTitle,
-  IonToolbar,
-} from '@ionic/react';
+import { IonList } from '@ionic/react';
 import { TMapList } from '../MapsPage';
 import { MapItem } from '../MapItem/MapItem';
+import { Caption } from '../../common/Caption/Caption';
 
 export type TMapListProps = {
   mapList: TMapList | undefined;
@@ -23,12 +14,7 @@ export const MapList = ({ mapList }: TMapListProps) => {
 
   return (
     <>
-      <IonTitle>
-        <IonButton>
-          <IonIcon name="arrow-back-circle-outline"></IonIcon>
-        </IonButton>
-        Maps
-      </IonTitle>
+      <Caption>Maps</Caption>
       <IonList>
         {mapList &&
           mapList.map((m) => <MapItem mapItem={m} key={m.id}></MapItem>)}
