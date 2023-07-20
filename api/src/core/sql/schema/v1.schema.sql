5376e7ce5e85370c97d9ef593effc3851cfd9b48
Michael Marshall <michaelmarshall220@gmail.com>
2023-07-20 08:08:28 -0500

    cleanup

--- api/src/core/sql/schema/v1.schema.sql
+++ api/src/core/sql/schema/v1.schema.sql
@@ -1,8 +1,7 @@
 -- schema.sql
 create schema IF NOT EXISTS "public";
 
-SET
-  search_path TO "public";
+SET search_path TO "public";
 -- GENERAL ------------------------------------------------------------
 
 CREATE OR REPLACE FUNCTION random_between(low INT ,high INT)
