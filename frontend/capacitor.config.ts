46ce0567e86cdeabc522a086f69d5eea54e085a1
Michael Marshall <michaelmarshall220@gmail.com>
2023-07-24 13:26:25 -0500

    setup first internal android release.

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'rocks.crowd',
   appName: 'frontend',
   webDir: 'dist',
   server: {
