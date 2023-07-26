d98215ba8d7cb6987881d6458c6302836ff9e0a7
hiroshi <hiroshi.tan57@gmail.com>
2023-07-25 20:45:29 -0400

    feat: add SiteTextListPage

---
+++ frontend/src/components/site-text/SiteTextListPage/styled.tsx
@@ -0,0 +1,27 @@
+import { styled } from 'styled-components';
+
+export const CaptainContainer = styled.div`
+  height: 50px;
+`;
+
+export const LangSelectorContainer = styled.div`
+  display: flex;
+  width: 100%;
+  padding: 0 16px;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const CardListContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  padding: 16px;
+  align-items: center;
+  justify-content: space-between;
+  gap: 16px;
+`;
+
+export const CardContainer = styled.div`
+  width: 47%;
+`;
