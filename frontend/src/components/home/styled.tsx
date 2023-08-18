20ee6216f66985211d8ef119b32dae935c467bd2
Elisabeth <shakespearee04@gmail.com>
2023-08-18 15:59:30 -0500

    restyle the home page so that switching from md to ios won't be such a big difference

--- frontend/src/components/home/styled.tsx
+++ frontend/src/components/home/styled.tsx
@@ -1,8 +1,23 @@
 import { styled } from 'styled-components';
 
-import { IonCard } from '@ionic/react';
+import { IonCard, IonCardTitle, IonItemGroup, IonLabel } from '@ionic/react';
 
 export const CustomIonCard = styled(IonCard)(() => ({
   width: '100%',
   padding: '20px',
+}));
+
+export const CustomGroup = styled(IonItemGroup)(() => ({
+  marginTop: '20px',
+}));
+
+export const CustomIonLabel = styled(IonLabel)(() => ({
+  marginLeft: '20px',
+  fontSize: '16px',
+  fontWeight: 'normal',
+  opacity: '50%',
+}));
+
+export const StIonCardTitleContainer = styled(IonCardTitle)(() => ({
+  marginTop: '0px',
 }));
