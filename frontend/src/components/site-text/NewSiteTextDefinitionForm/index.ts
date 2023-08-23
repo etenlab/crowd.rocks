7b97b184a752ec08382d4ed5c978af4c8f575346
hiroshi <hiroshi.tan57@gmail.com>
2023-08-22 17:07:15 -0700

    feat: add NewSiteTextDefinitionForm component

---
+++ frontend/src/components/site-text/NewSiteTextDefinitionForm/index.ts
@@ -0,0 +1,1 @@
+export { NewSiteTextDefinitionForm } from './NewSiteTextDefinitionForm';
---
+++ frontend/src/hooks/useSiteTextUpsertMutation.ts
@@ -0,0 +1,52 @@
+import { useIonToast } from '@ionic/react';
+
+import { useSiteTextUpsertMutation as useGeneratedSiteTextUpsertMutation } from '../generated/graphql';
+
+import { ErrorType } from '../generated/graphql';
+
+import { updateCacheWithUpsertSiteText } from '../cacheUpdators/upsertSiteText';
+
+import { useTr } from '../hooks/useTr';
+
+export function useSiteTextUpsertMutation() {
+  const { tr } = useTr();
+  const [present] = useIonToast();
+
+  return useGeneratedSiteTextUpsertMutation({
+    update(cache, { data: upsertData, errors }) {
+      if (
+        !errors &&
+        upsertData &&
+        upsertData.siteTextUpsert.error === ErrorType.NoError
+      ) {
+        const newSiteTextDefinition =
+          upsertData.siteTextUpsert.site_text_definition;
+
+        if (!newSiteTextDefinition) {
+          return;
+        }
+
+        updateCacheWithUpsertSiteText(cache, newSiteTextDefinition);
+
+        present({
+          message: tr('Success at creating new site text!'),
+          duration: 1500,
+          position: 'top',
+          color: 'success',
+        });
+      } else {
+        console.log('useSiteTextUpsertMutation: ', errors);
+        console.log(upsertData?.siteTextUpsert.error);
+
+        present({
+          message: `${tr('Failed at creating new site text!')} [${
+            upsertData?.siteTextUpsert.error || ''
+          }]`,
+          duration: 1500,
+          position: 'top',
+          color: 'danger',
+        });
+      }
+    },
+  });
+}
