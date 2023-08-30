96e9b74b52d2597655a5d255a4baa1422f2aa510
hiroshi <hiroshi.tan57@gmail.com>
2023-08-30 16:56:54 -0400

    feat: add GoogleTranslationPage

---
+++ frontend/src/components/translation/GoogleTranslationPage/styled.ts
@@ -0,0 +1,8 @@
+import { styled } from 'styled-components';
+
+export const ResultBoard = styled.div`
+  padding: 20px;
+  background: #e8f5f5;
+  border: 1px solid #b1d5c7;
+  border-radius: 10px;
+`;
