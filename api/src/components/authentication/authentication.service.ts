5f1f488b7b5ba59722e972bb08755d359a2feaa1
Ihor_S <ihor.shcherbyna@clockwise.software>
2023-09-08 18:24:43 +0200

    chore: remove garbage

--- api/src/components/authentication/authentication.service.ts
+++ api/src/components/authentication/authentication.service.ts
@@ -1,23 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { PostgresService } from '../../core/postgres.service';
-import { getAdminTokenSQL } from '../../core/sql-string';
 
 @Injectable()
 export class AuthenticationService {
   constructor(private pg: PostgresService) {}
-
-  // TODO: delete this service method for mocking user when read user registration & tokens
-  // async getAdminToken(): Promise<string | null> {
-  //   try {
-  //     const res = await this.pg.pool.query(...getAdminTokenSQL());
-
-  //     return res.rows[0].token;
-  //   } catch (e) {
-  //     console.error(e);
-  //   }
-
-  //   return null;
-  // }
 
   async get_user_id_from_bearer(token: string): Promise<number | null> {
     const res1 = await this.pg.pool.query(
