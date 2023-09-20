960e3fee7e1820cb27261952d2af2871e84a68dc
hiroshi <hiroshi.tan57@gmail.com>
2023-09-20 12:16:54 -0400

    feat: add e2e test for admin login

--- api/test/app.e2e-spec.ts
+++ api/test/app.e2e-spec.ts
@@ -2,8 +2,9 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { ErrorType } from 'src/common/types';
 
-describe('AppController (e2e)', () => {
+describe('GraphQL (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
@@ -15,10 +16,45 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterEach(async () => {
+    await app.close();
+  });
+
+  it('Test admin login mutation', async () => {
+    const queryObj = {
+      operationName: 'Login',
+      variables: {
+        email: 'admin@crowd.rocks',
+        password: 'asdfasdf',
+      },
+      query: `
+        mutation Login($email: String!, $password: String!) {
+          login(input: {email: $email, password: $password}) {
+                error
+                session {
+                  ...SessionFields
+                  __typename
+              }
+              __typename
+          }
+        }
+        fragment SessionFields on Session {
+            user_id
+            token
+            avatar
+            avatar_url
+            __typename
+        }
+      `,
+    };
+
+    const res = await request(app.getHttpServer())
+      .post('/graphql')
+      .send(queryObj);
+
+    expect(res.status).toEqual(200);
+    expect(res.body.data.login.error).toEqual(ErrorType.NoError);
+    expect(res.body.data.login.session.user_id).toEqual('1');
+    expect(res.body.data.login.session.avatar).toEqual('Admin');
   });
 });
