75b8a1b221542334421321b5508fedbd53ea83b9
Elisabeth <shakespearee04@gmail.com>
2023-10-03 10:21:21 -0500

    restructure file/folder architecture for translations; add translation vote-reset scripts and vote-reset batch scripts

---
+++ api/src/core/sql/translation/phrase_to_phrase/batch_ranslation_vote_reset.sql
@@ -0,0 +1,44 @@
+create or replace procedure batch_phrase_to_phrase_translation_vote_reset(
+  in p_phrase_to_phrase_translation_ids bigint[],
+  in p_token varchar(512),
+  inout p_phrase_to_phrase_translation_vote_ids bigint[],
+  inout p_error_types varchar(32)[],
+  inout p_error_type varchar(32)
+)
+language plpgsql
+as $$
+declare
+  v_phrase_to_phrase_translation_ids_length int;
+
+  v_temp_phrase_to_phrase_translation_vote_id bigint;
+  v_temp_error_type varchar(32);
+begin
+  -- validate inputs
+
+  v_phrase_to_phrase_translation_ids_length := array_length(
+    p_phrase_to_phrase_translation_ids::bigint[], 1
+  );
+
+  p_phrase_to_phrase_translation_vote_ids := array[]::bigint[];
+  p_error_types := array[]::varchar(32)[];
+
+  for i in 1..v_phrase_to_phrase_translations_ids_length loop
+    v_temp_phrase_to_phrase_translation_vote_id := 0;
+    v_temp_error_type := 'NoError';
+
+    call phrase_to_phrase_translation_vote_reset(
+      p_phrase_to_phrase_translation_ids[i],
+      p_token,
+      v_temp_phrase_to_phrase_translation_vote_id,
+      v_temp_error_type
+    );
+
+    p_phrase_to_phrase_translation_vote_ids := array_append(
+      p_phrase_to_phrase_translation_vote_ids, 
+      v_temp_phrase_to_phrase_translation_vote_id
+    );
+    p_error_types := array_append(p_error_types, v_temp_error_type);
+  end loop;
+
+  p_error_type := 'NoError';
+end; $$;
---
+++ api/src/core/sql/translation/word_to_word/translation_vote_reset.sql
@@ -0,0 +1,66 @@
+create or replace procedure word_to_word_translation_vote_reset(
+  in p_word_to_word_translation_id bigint,
+  in p_token varchar(512),
+  inout p_word_to_word_translations_vote_id bigint,
+  inout p_error_type varchar(32)
+)
+language plpgsql
+as $$
+declare
+  v_user_id bigint;
+  v_word_to_word_translation_id bigint;
+begin
+  p_error_type := 'UnknownError';
+
+  -- validate user
+  select user_id
+  into v_user_id
+  from tokens
+  where token = p_token;
+
+  if v_user_id is null then
+    p_error_type := 'Unauthorized';
+    return;
+  end if;
+
+  -- validate inpus
+  if p_word_to_word_translation_id is null then
+    p_error_type := 'InvalidInputs';
+    return;
+  end if;
+
+  -- check for word_to_word_translation existance
+  v_word_to_word_translation_id := null;
+
+  select word_to_word_translation_id
+  from word_to_word_translations
+  where word_to_word_translation_id = p_word_to_word_translation_id
+  into v_word_to_word_translation_id;
+
+  if v_word_to_word_translation_id is null then
+    p_error_type := 'WordToWordTranslationNotFound';
+    return;
+  end if;
+
+  insert into word_to_word_translations_votes(word_to_word_translation_id, user_id, vote)
+  values (p_word_to_word_translation_id, v_user_id, null)
+  on conflict (word_to_word_translation_id, user_id)
+  do update set vote = EXCLUDED.vote
+  returning word_to_word_translations_vote_id
+  into p_word_to_word_translations_vote_id;
+
+  if p_word_to_word_translations_vote_id is null then
+    select word_to_word_translations_vote_id
+    from word_to_word_translations_votes
+    where word_to_word_translation_id = p_word_to_word_translation_id
+      and user_id = v_user_id
+    into p_word_to_word_translations_vote_id;
+  end if;
+
+  if p_word_to_word_translations_vote_id is null then
+    return;
+  end if;
+  
+  p_error_type := 'NoError';
+
+end; $$;
