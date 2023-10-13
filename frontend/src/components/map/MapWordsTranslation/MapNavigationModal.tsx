36eb0d2b894d37cbe6e448235bd90026013cc064
hiroshi <hiroshi.tan57@gmail.com>
2023-10-13 17:43:00 -0400

    feat: add Modals for MapWordOrPhrase List Page
    MapFilterModal
    MapNavigationModal

---
+++ frontend/src/components/map/MapWordsTranslation/MapNavigationModal.tsx
@@ -0,0 +1,49 @@
+import { Stack, Typography, Divider, Button, IconButton } from '@mui/material';
+
+import { useTr } from '../../../hooks/useTr';
+
+import { useAppContext } from '../../../hooks/useAppContext';
+import { Cancel } from '../../common/icons/Cancel';
+
+export function MapNavigationModal() {
+  const { tr } = useTr();
+  const {
+    actions: { setModal },
+  } = useAppContext();
+
+  const handleCancel = () => {
+    setModal(null);
+  };
+
+  const chars = [...new Array(26)]
+    .map((_, index) => index)
+    .map((num) => String.fromCharCode('A'.charCodeAt(0) + num));
+
+  return (
+    <Stack gap="24px">
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
+        <Typography variant="h2">{tr('Go to')}...</Typography>
+        <IconButton onClick={handleCancel}>
+          <Cancel sx={{ fontSize: 24 }} color="dark" />
+        </IconButton>
+      </Stack>
+      <Divider />
+      <Stack direction="row" gap="10px" flexWrap="wrap">
+        {chars.map((item) => (
+          <Button
+            key={item}
+            variant="contained"
+            color="gray_bg"
+            sx={{
+              padding: '7px',
+              minWidth: '37px',
+              border: (theme) => `1px solid ${theme.palette.text.gray_stroke}`,
+            }}
+          >
+            {item}
+          </Button>
+        ))}
+      </Stack>
+    </Stack>
+  );
+}
