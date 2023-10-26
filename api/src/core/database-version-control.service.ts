69dafaf851cc28d92ffd97b9444f1e02b346477d
Elisabeth <shakespearee04@gmail.com>
2023-10-25 21:21:27 -0500

    add dbversion update function for v7

--- api/src/core/database-version-control.service.ts
+++ api/src/core/database-version-control.service.ts
@@ -50,6 +50,9 @@ export class DatabaseVersionControlService {
         case 5:
           console.log('Updating database to version 6');
           await this.loadVersion6();
+        case 6:
+          console.log('Updating database to version 7');
+          await this.loadVersion7();
         default:
           console.error('Database version is current');
       }
@@ -423,6 +426,11 @@ export class DatabaseVersionControlService {
 
     // set version
     await this.setVersionNumber(6);
+  }
+
+  async loadVersion7(): Promise<void> {
+    // set version
+    await this.setVersionNumber(7);
   }
 
   async registerUser(
