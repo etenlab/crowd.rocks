79b24f66da151edaba24b344776220b1d9398f99
Ihor_S <ihor.shcherbyna@clockwise.software>
2023-11-01 13:45:36 +0100

    fix: materialized view to represent all translations

--- api/src/core/sql/schema/final.schema.sql
+++ api/src/core/sql/schema/final.schema.sql
@@ -736,7 +736,7 @@ create table answers (
 
 
 CREATE materialized view  mv_words_languages as
-select distinct 
+select
   w.word_id,
   w2.language_code as t_language_code,
   w2.dialect_code as t_dialect_code,
@@ -749,11 +749,11 @@ select distinct
   join word_definitions wd2 on wd2.word_definition_id = wtwt.to_word_definition_id 
   join words w2 on w2.word_id = wd2.word_id
 union all 
-select distinct
+select
   w.word_id,
-  w3.language_code as t_language_code,
-  w3.dialect_code as t_dialect_code,
-  w3.geo_code as t_geo_code,
+  w2.language_code as t_language_code,
+  w2.dialect_code as t_dialect_code,
+  w2.geo_code as t_geo_code,
   null::numeric  as t_word_id,
   p2.phrase_id as t_phrase_id
   from words w 
@@ -761,35 +761,39 @@ select distinct
   join word_to_phrase_translations wtpt on wtpt.from_word_definition_id = wd.word_definition_id 
   join phrase_definitions pd on pd.phrase_definition_id = wtpt.to_phrase_definition_id
   join phrases p2 on pd.phrase_id = p2.phrase_id 
-  join words w3 on w3.word_id = p2.words[1];
+  join words w2 on w2.word_id = p2.words[1];
 
-CREATE UNIQUE INDEX mv_words_languages_unq_idx ON mv_words_languages USING btree (word_id, t_language_code, t_dialect_code, t_geo_code, t_word_id,t_phrase_id) nulls not DISTINCT;
+CREATE INDEX mv_words_languages_idx ON mv_words_languages USING btree (word_id, t_language_code, t_dialect_code, t_geo_code, t_word_id,t_phrase_id) nulls not DISTINCT;
 CREATE INDEX mv_words_languages_full_language_code_idx ON mv_words_languages USING btree (t_language_code, t_dialect_code, t_geo_code);
 CREATE INDEX mv_words_languages_language_code_idx ON mv_words_languages USING btree (t_language_code);
 CREATE INDEX mv_words_languages_t_phrase_id_idx ON mv_words_languages USING btree (t_phrase_id);
 CREATE INDEX mv_words_languages_t_word_id_idx ON mv_words_languages USING btree (t_word_id);
 
+-- mv_phrases_languages definition
+
+DROP materialized view  if exists mv_phrases_languages;
+
 CREATE materialized view  mv_phrases_languages as 
-select distinct 
+select 
   p.phrase_id,
-  w.language_code as t_language_code,
-  w.dialect_code as t_dialect_code,
-  w.geo_code as t_geo_code,
-  w.word_id as t_word_id,
+  w2.language_code as t_language_code,
+  w2.dialect_code as t_dialect_code,
+  w2.geo_code as t_geo_code,
+  w2.word_id as t_word_id,
   null::numeric  as t_phrase_id
   from phrases p
   join phrase_definitions pd  on p.phrase_id = pd.phrase_id  
   join phrase_to_word_translations ptwt on ptwt.from_phrase_definition_id = pd.phrase_definition_id
   join word_definitions wd2 on wd2.word_definition_id = ptwt.to_word_definition_id 
-  join words w on w.word_id = wd2.word_id
+  join words w2 on w2.word_id = wd2.word_id
 union all 
-select distinct
+select
   p.phrase_id,
   w2.language_code as t_language_code,
   w2.dialect_code as t_dialect_code,
   w2.geo_code as t_geo_code,
   null::numeric  as t_word_id,
-  p.phrase_id as t_phrase_id
+  p2.phrase_id as t_phrase_id
   from phrases p
   join phrase_definitions pd2 on p.phrase_id = pd2.phrase_id
   join phrase_to_phrase_translations ptpt on ptpt.from_phrase_definition_id = pd2.phrase_definition_id  
@@ -797,7 +801,7 @@ select distinct
   join phrases p2 on pd3.phrase_id = p2.phrase_id 
   join words w2 on w2.word_id = p2.words[1];
 
-CREATE UNIQUE INDEX mv_phrases_languages_unq_idx ON mv_phrases_languages USING btree (phrase_id, t_language_code, t_dialect_code, t_geo_code, t_word_id,t_phrase_id) nulls not DISTINCT;
+CREATE INDEX mv_phrases_languages_idx ON mv_phrases_languages USING btree (phrase_id, t_language_code, t_dialect_code, t_geo_code, t_word_id,t_phrase_id) nulls not DISTINCT;
 CREATE INDEX mv_phrases_languages_full_language_code_idx ON mv_phrases_languages USING btree (t_language_code, t_dialect_code, t_geo_code);
 CREATE INDEX mv_phrases_languages_language_code_idx ON mv_phrases_languages USING btree (t_language_code);
 CREATE INDEX mv_phrases_languages_t_phrase_id_idx ON mv_phrases_languages USING btree (t_phrase_id);
