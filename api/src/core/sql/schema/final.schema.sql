1915396fc0bb8d7b24204c47c1ea3f8fd7aab886
Ihor_S <siafin2010@gmail.com>
2023-11-09 10:19:33 +0100

    fix: add map_votest table definitions to final.schema

--- api/src/core/sql/schema/final.schema.sql
+++ api/src/core/sql/schema/final.schema.sql
@@ -829,19 +829,30 @@ CREATE INDEX mv_phrases_languages_language_code_idx ON mv_phrases_languages USIN
 CREATE INDEX mv_phrases_languages_t_phrase_id_idx ON mv_phrases_languages USING btree (t_phrase_id);
 CREATE INDEX mv_phrases_languages_t_word_id_idx ON mv_phrases_languages USING btree (t_word_id);
 
----
-ALTER TABLE public.original_maps_votes DROP CONSTRAINT if exists words_votes_map_id_fkey;
-ALTER TABLE public.original_maps_votes DROP CONSTRAINT if exists original_map_votes_id_fkey;
+-- public.original_maps_votes definition
+CREATE TABLE public.original_maps_votes (
+	maps_vote_id bigserial NOT NULL,
+	user_id int8 NOT NULL,
+	map_id int8 NOT NULL,
+	vote bool NULL,
+	last_updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	CONSTRAINT original_maps_votes_pkey PRIMARY KEY (maps_vote_id),
+	CONSTRAINT original_maps_votes_user_id_word_id_key UNIQUE (user_id, map_id)
+);
+CREATE INDEX idx__map_id__original_maps_votes ON public.original_maps_votes USING btree (map_id);
 ALTER TABLE public.original_maps_votes ADD CONSTRAINT original_map_votes_id_fkey FOREIGN KEY (map_id) REFERENCES public.original_maps(original_map_id) ON DELETE CASCADE;
+ALTER TABLE public.original_maps_votes ADD CONSTRAINT original_map_votes_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(user_id) ON DELETE CASCADE;
 
-ALTER TABLE public.translated_maps_votes DROP CONSTRAINT if exists words_votes_map_id_fkey;
-ALTER TABLE public.translated_maps_votes DROP CONSTRAINT if exists translated_map_votes_fkey;
+-- public.translated_maps_votes definition
+CREATE TABLE public.translated_maps_votes (
+	maps_vote_id bigserial NOT NULL,
+	user_id int8 NOT NULL,
+	map_id int8 NOT NULL,
+	vote bool NULL,
+	last_updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	CONSTRAINT translated_maps_votes_pkey PRIMARY KEY (maps_vote_id),
+	CONSTRAINT translated_maps_votes_user_id_word_id_key UNIQUE (user_id, map_id)
+);
+CREATE INDEX idx__map_id__translated_maps_votes ON public.translated_maps_votes USING btree (map_id);
 ALTER TABLE public.translated_maps_votes ADD CONSTRAINT translated_map_votes_fkey FOREIGN KEY (map_id) REFERENCES public.translated_maps(translated_map_id) ON DELETE CASCADE;
----
-ALTER TABLE public.original_maps_votes DROP CONSTRAINT if exists words_votes_user_id_fkey;
-ALTER TABLE public.original_maps_votes DROP CONSTRAINT if exists original_map_votes_user_id_fkey;
-ALTER TABLE public.original_maps_votes ADD CONSTRAINT original_map_votes_user_id_fkey FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE;
-
-ALTER TABLE public.translated_maps_votes DROP CONSTRAINT if exists words_votes_user_id_fkey;
-ALTER TABLE public.translated_maps_votes DROP CONSTRAINT if exists translated_map_votes_user_id_fkey;
-ALTER TABLE public.translated_maps_votes ADD CONSTRAINT translated_map_votes_user_id_fkey FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE;;
+ALTER TABLE public.translated_maps_votes ADD CONSTRAINT translated_map_votes_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(user_id) ON DELETE CASCADE;
