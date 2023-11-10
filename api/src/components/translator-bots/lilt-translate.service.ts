1ab211eceeb3de4323bd69a51109c931de5ddd2b
Ihor_S <siafin2010@gmail.com>
2023-11-10 17:05:10 +0100

    feat: try lilt translation of composed string

--- api/src/components/translator-bots/lilt-translate.service.ts
+++ api/src/components/translator-bots/lilt-translate.service.ts
@@ -12,15 +12,16 @@ import {
   LanguageListForBotTranslateOutput,
 } from './types';
 import fetch, { Headers } from 'node-fetch';
-import { getLangsRegistry } from '../../../../utils';
+import { getLangsRegistry, languageInput2tag } from '../../../../utils';
 
 const LIMIT_REQUESTS = 4000; // per LIMIT_TIME
 const LIMIT_TIME = 60 * 1000; // per minute
 const WAIT_TIMEOUT = 60 * 1000; // miliseconds; if limit_requests reached, need to wait 60 seconds before the next request
 const LIMIT_LENGTH = 5000; // characters of a source string per request
-const JOINER = '.<br/>'; // joins words or phrases before sending to lilt api
+const JOINER = '<br/>'; // joins words or phrases before sending to lilt api
 
 const LILT_BOT_EMAIL = 'liltbot@crowd.rocks';
+const LILT_API_URL = 'https://lilt.com/2';
 
 type TResLangs = {
   source_to_target: {
@@ -29,6 +30,47 @@ type TResLangs = {
   code_to_name: {
     [key: string]: string;
   };
+};
+
+type TLiltMemoryObject = {
+  id: number;
+  srclang: string;
+  trglang: string;
+  srclocale: string;
+  trglocale: string;
+  name: string;
+  version: number;
+  created_at: number;
+  updated_at: number;
+  resources: string[];
+};
+
+type TLiltMemoryCreateBody = {
+  name: string;
+  srclang: string;
+  trglang: string;
+  srclocale?: string;
+  trglocale?: string;
+};
+
+type TlilitTranslateResponce = {
+  untokenizedSource: string;
+  tokenizedSource: string;
+  sourceDelimiters: string[];
+  translation: Array<
+    [
+      {
+        score: number;
+        align: string;
+        targetDelimiters: string[];
+        targetWords: string[];
+        target: string;
+        targetWithTags: string;
+        isTMMatch: boolean;
+        provenance: string;
+      },
+    ]
+  >;
 };
 
 @Injectable()
@@ -43,21 +85,115 @@ export class LiltTranslateService implements ITranslator {
     this.availableRequests = LIMIT_REQUESTS;
   }
 
-  async liltTranslate(
+  private async liltTranslateApiCall(
     origStr,
-    from: LanguageInput,
-    to: LanguageInput,
-  ): Promise<string> {
-    //todo
-    return '';
+    memoryId: string,
+  ): Promise<string | null> {
+    const strEncoded = encodeURIComponent(origStr);
+    const url = `${LILT_API_URL}/translate?key=${this.config.LILT_KEY}&memory_id=${memoryId}&source="${strEncoded}"`;
+    const res = await fetch(url, {
+      method: 'GET',
+    });
+    const liltCrerateMemoryRes = (await res.json()) as TlilitTranslateResponce;
+    if (!liltCrerateMemoryRes?.translation[0][0].target) {
+      Logger.error(
+        `liltTranslateService#liltCreateMemoryIdApiCall: ${JSON.stringify(
+          liltCrerateMemoryRes,
+        )}`,
+      );
+      return null;
+    }
+    return liltCrerateMemoryRes.translation[0][0].target;
   }
 
-  processApiCall = async (
+  private liltMemoryName(
+    fromLang: LanguageInput,
+    toLang: LanguageInput,
+  ): string {
+    const s_langFullTag = languageInput2tag(fromLang);
+    const t_langFullTag = languageInput2tag(toLang);
+    return `liltbot@crowd.rocks-${s_langFullTag}-${t_langFullTag}`;
+  }
+
+  private async liltGetMemoriesApiCall(): Promise<TLiltMemoryObject[]> {
+    const url = `${LILT_API_URL}/memories?key=${this.config.LILT_KEY}`;
+    const res = await fetch(url, {
+      method: 'GET',
+    });
+    const liltMemoriesRes = await res.json();
+    if (!(liltMemoriesRes?.length > 0)) {
+      Logger.error(
+        `liltTranslateService#liltGetMemoryIdApiCall: ${JSON.stringify(
+          liltMemoriesRes,
+        )}`,
+      );
+      return [];
+    }
+    return liltMemoriesRes as TLiltMemoryObject[];
+  }
+
+  private async liltCreateMemoryIdApiCall(
+    fromLang: LanguageInput,
+    toLang: LanguageInput,
+  ): Promise<TLiltMemoryObject | null> {
+    const reqBody: TLiltMemoryCreateBody = {
+      name: this.liltMemoryName(fromLang, toLang),
+      srclang: fromLang.language_code,
+      trglang: toLang.language_code,
+    };
+    if (fromLang.geo_code) {
+      reqBody.srclocale = fromLang.geo_code;
+    }
+    if (toLang.geo_code) {
+      reqBody.trglocale = toLang.geo_code;
+    }
+    const body = JSON.stringify(reqBody);
+
+    const url = `${LILT_API_URL}/memories?key=${this.config.LILT_KEY}`;
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('Accept', 'application/json');
+
+    const res = await fetch(url, {
+      method: 'POST',
+      headers,
+      body,
+    });
+
+    const liltCrerateMemoryRes = await res.json();
+    if (!liltCrerateMemoryRes?.id) {
+      Logger.error(
+        `liltTranslateService#liltCreateMemoryIdApiCall: ${JSON.stringify(
+          liltCrerateMemoryRes,
+        )}`,
+      );
+      return null;
+    }
+    return liltCrerateMemoryRes as TLiltMemoryObject;
+  }
+
+  private async getLiltMemoryId(
+    fromLang: LanguageInput,
+    toLang: LanguageInput,
+  ): Promise<string | undefined> {
+    const existingMemories = await this.liltGetMemoriesApiCall();
+    const foundMemory = existingMemories.find(
+      (memory) => memory.name === this.liltMemoryName(fromLang, toLang),
+    );
+    if (foundMemory?.id) return String(foundMemory.id);
+
+    const newMemory = await this.liltCreateMemoryIdApiCall(fromLang, toLang);
+    if (newMemory?.id) return String(newMemory.id);
+
+    return undefined;
+  }
+
+  private planLiltTranslationApiCalls = async (
     chunks: Array<string>,
-    from: LanguageInput,
-    to: LanguageInput,
+    memoryId: string,
   ): Promise<Array<string>> => {
     if (!(chunks?.length > 0)) return [];
+
     if (
       Date.now() - this.firstOperateTime < 60 * LIMIT_TIME &&
       this.availableRequests < 1
@@ -66,7 +202,12 @@ export class LiltTranslateService implements ITranslator {
       this.availableRequests = LIMIT_REQUESTS;
     }
 
-    const translation = await this.liltTranslate(chunks.join(JOINER), from, to);
+    const translation = await this.liltTranslateApiCall(
+      chunks.join(JOINER),
+      memoryId,
+    );
+    if (!translation)
+      throw new Error(`Lilt translation API returned nullish value`);
     const translatedChunks = translation.split(JOINER);
 
     this.lastOperateTime = Date.now();
@@ -74,10 +215,10 @@ export class LiltTranslateService implements ITranslator {
     return translatedChunks;
   };
 
-  translate = async (
+  public translate = async (
     texts: string[],
-    from: LanguageInput,
-    to: LanguageInput,
+    fromLang: LanguageInput,
+    toLang: LanguageInput,
   ): Promise<string[]> => {
     try {
       texts.forEach((text) => {
@@ -91,11 +232,18 @@ export class LiltTranslateService implements ITranslator {
       let chunks: string[] = [];
       const translatedTexts: string[] = [];
       let availableCharacters = LIMIT_LENGTH;
+      const memoryId = await this.getLiltMemoryId(fromLang, toLang);
+      if (!memoryId) {
+        Logger.error(
+          `liltTranslateService#translate: lilt memory Id is undefined`,
+        );
+        return [];
+      }
 
       for (const text of texts) {
         if (availableCharacters < text.length + JOINER.length) {
           translatedTexts.push(
-            ...(await this.processApiCall(chunks, from, to)),
+            ...(await this.planLiltTranslationApiCalls(chunks, memoryId)),
           );
           chunks = [];
           availableCharacters = LIMIT_LENGTH;
@@ -104,7 +252,9 @@ export class LiltTranslateService implements ITranslator {
         availableCharacters = availableCharacters - text.length - JOINER.length;
       }
       // last one
-      translatedTexts.push(...(await this.processApiCall(chunks, from, to)));
+      translatedTexts.push(
+        ...(await this.planLiltTranslationApiCalls(chunks, memoryId)),
+      );
 
       return translatedTexts;
     } catch (err) {
@@ -113,10 +263,10 @@ export class LiltTranslateService implements ITranslator {
     }
   };
 
-  async getLanguages(): Promise<LanguageListForBotTranslateOutput> {
+  public async getLanguages(): Promise<LanguageListForBotTranslateOutput> {
     try {
       const ourLangTags = (await getLangsRegistry()).langs.map((l) => l.tag);
-      const url = `https://lilt.com/2/languages?key=${this.config.LILT_KEY}`;
+      const url = `${LILT_API_URL}/languages?key=${this.config.LILT_KEY}`;
       const res = await fetch(url, {
         method: 'GET',
       });
@@ -167,7 +317,10 @@ export class LiltTranslateService implements ITranslator {
     }
   }
 
-  getTranslatorToken = async (): Promise<{ id: string; token: string }> => {
+  public getTranslatorToken = async (): Promise<{
+    id: string;
+    token: string;
+  }> => {
     // // check if token for liltbot exists
     const tokenRes = await this.pg.pool.query(
       `select t.token, u.user_id
