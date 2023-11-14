b9246f1f1d5c8ada5077dab70d3daab70086eec8
hiroshi <hiroshi.tan57@gmail.com>
2023-11-14 17:27:53 -0500

    feat: add IonContentScrollElement to global states

--- frontend/src/reducers/components.actions.ts
+++ frontend/src/reducers/components.actions.ts
@@ -3,6 +3,7 @@ import { ReactNode } from 'react';
 export const actions = {
   ADD_MODAL: 'ADD_MODAL',
   REMOVE_MODAL: 'REMOVE_MODAL',
+  SET_ION_CONTENT_SCROLL_ELEMENT: 'SET_ION_CONTENT_SCROLL_ELEMENT',
 };
 
 export function addModal(
@@ -24,5 +25,12 @@ export function removeModal(id: string) {
   return {
     type: actions.REMOVE_MODAL,
     payload: id,
+  };
+}
+
+export function setIonContentScrollElement(scrollElement: HTMLElement | null) {
+  return {
+    type: actions.SET_ION_CONTENT_SCROLL_ELEMENT,
+    payload: scrollElement,
   };
 }
