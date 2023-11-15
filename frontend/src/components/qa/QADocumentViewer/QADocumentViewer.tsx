4ef89ff4a85b5af36fdd9e1126fff480364d13a0
hiroshi <hiroshi.tan57@gmail.com>
2023-11-15 14:11:58 -0500

    wip: refactor QADocumentViewer

--- frontend/src/components/qa/QADocumentViewer/QADocumentViewer.tsx
+++ frontend/src/components/qa/QADocumentViewer/QADocumentViewer.tsx
@@ -1,20 +1,7 @@
-import {
-  useMemo,
-  useState,
-  useCallback,
-  useRef,
-  useEffect,
-  ReactNode,
-} from 'react';
-import {
-  IonContent,
-  IonHeader,
-  IonModal,
-  IonTitle,
-  IonToolbar,
-  IonPopover,
-  useIonToast,
-} from '@ionic/react';
+import { useMemo, useState, useCallback, ReactNode, MouseEvent } from 'react';
+import { useParams, useHistory } from 'react-router';
+import { useIonToast } from '@ionic/react';
+import { Popover } from '@mui/material';
 
 import { ViewMode } from '../../documents/DocumentViewer/DocumentViewer';
 import { Dot } from '../../documents/DocumentViewer/styled';
@@ -25,18 +12,19 @@ import {
   QuestionOnWordRange,
   ErrorType,
 } from '../../../generated/graphql';
-import { useCreateQuestionOnWordRangeMutation } from '../../../hooks/useCreateQuestionOnWordRangeMutation';
-import { useUpsertAnswerMutation } from '../../../hooks/useUpsertAnswerMutation';
+// import { useUpsertAnswerMutation } from '../../../hooks/useUpsertAnswerMutation';
 
 import { useTr } from '../../../hooks/useTr';
+import { useAppContext } from '../../../hooks/useAppContext';
 
-import { QuestionForm } from './QuestionForm';
-import { AnswerList } from './AnswerList';
-import { QuestionsMenu } from './QuestionsMenu';
+// import { AnswerList } from './AnswerList';
+import { PieceOfTextModal } from './PieceOfTextModal';
+import { QuestionAddButton } from './QuestionAddButton';
 
 export type RangeItem = {
   entryId: string;
   order: number;
+  element: HTMLElement | null;
 };
 
 type QADocumentViewerProps = {
@@ -45,7 +33,18 @@ type QADocumentViewerProps = {
 };
 
 export function QADocumentViewer({ documentId, mode }: QADocumentViewerProps) {
+  const history = useHistory();
+  const { nation_id, language_id, cluster_id, document_id } = useParams<{
+    nation_id: string;
+    language_id: string;
+    cluster_id: string;
+    document_id: string;
+  }>();
+
   const { tr } = useTr();
+  const {
+    actions: { createModal, setNewQuestionFormPageData },
+  } = useAppContext();
   const [presetToast] = useIonToast();
 
   const { data, error } = useGetQuestionOnWordRangesByDocumentIdQuery({
@@ -53,27 +52,17 @@ export function QADocumentViewer({ documentId, mode }: QADocumentViewerProps) {
       document_id: documentId,
     },
   });
-  const [createQuestionOnWordRange] = useCreateQuestionOnWordRangeMutation();
-  const [upsertAnswer] = useUpsertAnswerMutation();
+  // const [upsertAnswer] = useUpsertAnswerMutation();
 
-  const popoverRef = useRef<HTMLIonPopoverElement>(null);
-
-  const [popoverOpen, setPopoverOpen] = useState(false);
-  const [isOpenModal, setIsOpenModal] = useState(false);
   const [range, setRange] = useState<{
     begin?: RangeItem;
     end?: RangeItem;
   }>({});
-  const [questions, setQuestions] = useState<QuestionOnWordRange[]>([]);
-  const [selectedQuestion, setSelectedQuestion] =
-    useState<QuestionOnWordRange | null>(null);
+  // const [selectedQuestion, setSelectedQuestion] =
+  //   useState<QuestionOnWordRange | null>(null);
   const [sentence, setSentence] = useState<string>('');
 
-  useEffect(() => {
-    if (range.begin && range.end && mode === 'edit') {
-      setIsOpenModal(true);
-    }
-  }, [range.begin, range.end, mode]);
+  const { openModal, closeModal } = createModal();
 
   const { dots, questionsMap } = useMemo(() => {
     const questionsMap = new Map<string, QuestionOnWordRange[]>();
@@ -129,7 +118,11 @@ export function QADocumentViewer({ documentId, mode }: QADocumentViewerProps) {
       if (arr.length > 0) {
         dots.push({
           entryId: key,
-          component: <Dot />,
+          component: (
+            <Dot
+              sx={{ backgroundColor: (theme) => theme.palette.background.red }}
+            />
+          ),
         });
       }
     }
@@ -139,70 +132,89 @@ export function QADocumentViewer({ documentId, mode }: QADocumentViewerProps) {
 
   const handleSelectQuestion = useCallback((question: QuestionOnWordRange) => {
     setRange({
-      begin: { entryId: question.begin.document_word_entry_id, order: 0 },
-      end: { entryId: question.end.document_word_entry_id, order: 0 },
+      begin: {
+        entryId: question.begin.document_word_entry_id,
+        order: 0,
+        element: null,
+      },
+      end: {
+        entryId: question.end.document_word_entry_id,
+        order: 0,
+        element: null,
+      },
     });
-    setSelectedQuestion(question);
-    setIsOpenModal(true);
-    setPopoverOpen(false);
+    // setSelectedQuestion(question);
   }, []);
 
-  const handleSelectRange = useCallback((entryId: string, index: number) => {
-    setRange((prev) => {
-      if (prev.begin?.entryId && prev.end?.entryId) {
-        // ...A... ... ...B...
-        if (prev.begin.entryId === entryId) {
-          // ...A(X)... ... ...B...
-          return {};
-        } else if (prev.end.entryId === entryId) {
-          // ...A... ... ...B(X)...
-          return { ...prev, end: undefined };
-        } else {
-          if (prev.begin.order <= index) {
-            return { ...prev, end: { entryId, order: index } };
+  const handleSelectRange = useCallback(
+    (entryId: string, index: number, e: MouseEvent<HTMLElement>) => {
+      setRange((prev) => {
+        if (prev.begin?.entryId && prev.end?.entryId) {
+          // ...A... ... ...B...
+          if (prev.begin.entryId === entryId) {
+            // ...A(X)... ... ...B...
+            return {};
+          } else if (prev.end.entryId === entryId) {
+            // ...A... ... ...B(X)...
+            return { ...prev, end: undefined };
+          } else {
+            if (prev.begin.order <= index) {
+              return {
+                ...prev,
+                end: { entryId, order: index, element: e.currentTarget },
+              };
+            }
+            // ...X... ... A... ... ...B...
           }
-          // ...X... ... A... ... ...B...
+        } else if (prev.begin?.entryId && !prev.end?.entryId) {
+          // ...A... ... ...
+          if (prev.begin.order <= index) {
+            return {
+              ...prev,
+              end: { entryId, order: index, element: e.currentTarget },
+            };
+          }
+          // ...X ... ... ... A ... ... ...
+        } else if (!prev.begin?.entryId && prev.end?.entryId) {
+          // ... ... ... B
+          return {};
+        } else if (!prev.begin?.entryId && !prev.end?.entryId) {
+          // ... ... ...
+          return {
+            begin: {
+              entryId,
+              order: index,
+              element: e.currentTarget,
+            },
+            end: undefined,
+          };
         }
-      } else if (prev.begin?.entryId && !prev.end?.entryId) {
-        // ...A... ... ...
-        if (prev.begin.order <= index) {
-          return { ...prev, end: { entryId, order: index } };
-        }
-        // ...X ... ... ... A ... ... ...
-      } else if (!prev.begin?.entryId && prev.end?.entryId) {
-        // ... ... ... B
-        return {};
-      } else if (!prev.begin?.entryId && !prev.end?.entryId) {
-        // ... ... ...
-        return {
-          begin: {
-            entryId,
-            order: index,
-          },
-          end: undefined,
-        };
-      }
 
-      return prev;
-    });
-  }, []);
+        return prev;
+      });
+    },
+    [],
+  );
 
   const handleSelectDot = useCallback(
-    (entryId: string, _index: number, e?: unknown) => {
-      setQuestions(questionsMap.get(entryId) || []);
-
-      popoverRef.current!.event = e;
-      setPopoverOpen(true);
+    (entryId: string) => {
+      openModal(
+        <PieceOfTextModal
+          questions={questionsMap.get(entryId) || []}
+          onSelectQuestion={handleSelectQuestion}
+          onClose={closeModal}
+        />,
+      );
     },
-    [questionsMap],
+    [closeModal, handleSelectQuestion, openModal, questionsMap],
   );
 
   const handleWordClick = useCallback(
-    (entryId: string, index: number, e?: unknown) => {
+    (entryId: string, index: number, e: MouseEvent<HTMLElement>) => {
       if (mode === 'view') {
-        handleSelectDot(entryId, index, e);
+        handleSelectDot(entryId);
       } else if (mode === 'edit') {
-        handleSelectRange(entryId, index);
+        handleSelectRange(entryId, index, e);
       }
     },
     [handleSelectRange, handleSelectDot, mode],
@@ -213,69 +225,35 @@ export function QADocumentViewer({ documentId, mode }: QADocumentViewerProps) {
   }, []);
 
   const handleCancel = () => {
-    setIsOpenModal(false);
     setRange({});
   };
 
-  const handleSaveQuestion = async (
-    question: string,
-    items: string[],
-    isMultiselect: boolean,
-  ) => {
-    if (!range.begin?.entryId || !range.end?.entryId) {
-      presetToast({
-        message: `${tr('Not selected range!')}`,
-        duration: 1500,
-        position: 'top',
-        color: 'danger',
-      });
+  // const handleSaveAnswer = async (answer: string, itemIds: string[]) => {
+  //   if (!selectedQuestion) {
+  //     presetToast({
+  //       message: `${tr('Not selected question!')}`,
+  //       duration: 1500,
+  //       position: 'top',
+  //       color: 'danger',
+  //     });
 
-      return;
-    }
+  //     return;
+  //   }
 
-    const res = await createQuestionOnWordRange({
-      variables: {
-        begin_document_word_entry_id: range.begin.entryId,
-        end_document_word_entry_id: range.end.entryId,
-        question: question,
-        question_items: items,
-        question_type_is_multiselect: isMultiselect,
-      },
-    });
+  //   const res = await upsertAnswer({
+  //     variables: {
+  //       question_id: selectedQuestion.question_id,
+  //       answer,
+  //       question_item_ids: itemIds,
+  //     },
+  //   });
 
-    if (res.data?.createQuestionOnWordRange.error !== ErrorType.NoError) {
-      return;
-    }
+  //   if (res.data?.upsertAnswers.error !== ErrorType.NoError) {
+  //     return;
+  //   }
 
-    handleCancel();
-  };
-
-  const handleSaveAnswer = async (answer: string, itemIds: string[]) => {
-    if (!selectedQuestion) {
-      presetToast({
-        message: `${tr('Not selected question!')}`,
-        duration: 1500,
-        position: 'top',
-        color: 'danger',
-      });
-
-      return;
-    }
-
-    const res = await upsertAnswer({
-      variables: {
-        question_id: selectedQuestion.question_id,
-        answer,
-        question_item_ids: itemIds,
-      },
-    });
-
-    if (res.data?.upsertAnswers.error !== ErrorType.NoError) {
-      return;
-    }
-
-    handleCancel();
-  };
+  //   handleCancel();
+  // };
 
   const documentRange = useMemo(
     () => ({
@@ -285,28 +263,67 @@ export function QADocumentViewer({ documentId, mode }: QADocumentViewerProps) {
     [range],
   );
 
-  const modalTitle = mode === 'edit' ? tr('New Question') : tr('Answers');
+  // modalContentCom =
+  //   mode === 'view' && selectedQuestion ? (
+  //     <AnswerList
+  //       onCancel={handleCancel}
+  //       onSave={handleSaveAnswer}
+  //       question={selectedQuestion}
+  //       sentence={sentence}
+  //     />
+  //   ) : (
+  //     modalContentCom
+  //   );
 
-  let modalContentCom =
-    mode === 'edit' ? (
-      <QuestionForm
-        onCancel={handleCancel}
-        onSave={handleSaveQuestion}
-        sentence={sentence}
-      />
+  const handleAddQuestionButton = useCallback(() => {
+    if (range.begin && range.end) {
+      setNewQuestionFormPageData({
+        range: {
+          begin: range.begin,
+          end: range.end,
+        },
+        sentence,
+      });
+      history.push(
+        `/${nation_id}/${language_id}/${cluster_id}/qa/new-question?document_id=${document_id}`,
+      );
+    }
+  }, [
+    cluster_id,
+    document_id,
+    history,
+    language_id,
+    nation_id,
+    range.begin,
+    range.end,
+    sentence,
+    setNewQuestionFormPageData,
+  ]);
+
+  const popoverCom =
+    range.begin && range.end && mode === 'edit' && range.begin.element ? (
+      <Popover
+        open={!!range.begin.element}
+        anchorEl={range.begin.element}
+        onClose={handleCancel}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+        sx={{
+          '& .MuiPopover-paper': {
+            marginTop: '2px',
+            borderRadius: '6px',
+          },
+        }}
+      >
+        <QuestionAddButton onClickAddButton={handleAddQuestionButton} />
+      </Popover>
     ) : null;
-
-  modalContentCom =
-    mode === 'view' && selectedQuestion ? (
-      <AnswerList
-        onCancel={handleCancel}
-        onSave={handleSaveAnswer}
-        question={selectedQuestion}
-        sentence={sentence}
-      />
-    ) : (
-      modalContentCom
-    );
 
   return (
     <>
@@ -319,25 +336,7 @@ export function QADocumentViewer({ documentId, mode }: QADocumentViewerProps) {
         onClickWord={handleWordClick}
       />
 
-      <IonPopover
-        ref={popoverRef}
-        isOpen={popoverOpen}
-        onDidDismiss={() => setPopoverOpen(false)}
-      >
-        <QuestionsMenu
-          questions={questions}
-          onSelectQuestion={handleSelectQuestion}
-        />
-      </IonPopover>
-
-      <IonModal isOpen={isOpenModal} onDidDismiss={() => setIsOpenModal(false)}>
-        <IonHeader>
-          <IonToolbar>
-            <IonTitle>{modalTitle}</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent className="ion-padding">{modalContentCom}</IonContent>
-      </IonModal>
+      {popoverCom}
     </>
   );
 }
