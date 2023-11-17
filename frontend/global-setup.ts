e824a56ed57f70972312dc015caefd3230058f30
Piyush Patel <ppatel@greentube.com>
2023-11-17 16:01:37 +0530

    Fixed flacky Global setup behavior on CI run.

--- frontend/global-setup.ts
+++ frontend/global-setup.ts
@@ -1,7 +1,6 @@
-import { chromium, expect } from '@playwright/test';
+import { chromium } from '@playwright/test';
 import RegistrationPage from './tests/pages/RegistrationPage';
 import RegisterData from './tests/data-factory/RegisterData';
-import HomePO from './tests/pages/HomePage';
 
 async function globalSetup() {
   const browser = await chromium.launch({ headless: true });
@@ -9,13 +8,11 @@ async function globalSetup() {
   const page = await context.newPage();
   const registerPage = new RegistrationPage(page);
   const validRegisterData = RegisterData.validRegisterData();
-  const homePage = new HomePO(page);
 
   await page.goto('http://localhost:3000/US/en/1/register');
   await registerPage.fillRegistrationForm(validRegisterData);
   await registerPage.clickOnRegisterButton();
-  expect(await homePage.isHomePageVisible()).toBeTruthy();
-
+  await page.waitForTimeout(5000);
   await page.context().storageState({ path: './registerAuth.json' });
   await browser.close();
 }
