5f0706db30330ed946a344eee9aac0566a86c163
hiroshi <hiroshi.tan57@gmail.com>
2023-11-20 19:10:22 -0500

    refactor: PieceOfTextModal component

--- frontend/src/components/qa/QADocumentViewer/PieceOfTextModal.tsx
+++ frontend/src/components/qa/QADocumentViewer/PieceOfTextModal.tsx
@@ -1,4 +1,7 @@
+import { useEffect, useMemo } from 'react';
 import {
+  Box,
+  CircularProgress,
   Stack,
   Typography,
   Divider,
@@ -9,23 +12,73 @@ import {
 } from '@mui/material';
 
 import { Cancel } from '../../common/icons/Cancel';
-
 import { useTr } from '../../../hooks/useTr';
-
-import { QuestionOnWordRange } from '../../../generated/graphql';
+import {
+  WordRangeInput,
+  QuestionOnWordRange,
+  useGetDocumentTextFromRangesLazyQuery,
+} from '../../../generated/graphql';
 
 type PieceOfTextModalProps = {
   questions: QuestionOnWordRange[];
-  onSelectQuestion(question: QuestionOnWordRange): void;
+  onSelectPiece(
+    pieceOfText: string,
+    startWordEntryId: string,
+    endWordEntryId: string,
+  ): void;
   onClose(): void;
 };
 
 export function PieceOfTextModal({
   questions,
-  onSelectQuestion,
+  onSelectPiece,
   onClose,
 }: PieceOfTextModalProps) {
   const { tr } = useTr();
+  const [getDocumentTextFromRange, { data, loading }] =
+    useGetDocumentTextFromRangesLazyQuery();
+
+  useEffect(() => {
+    const pieceOfTextsMap = new Map<string, boolean>();
+
+    for (const question of questions) {
+      const begin = question.begin.document_word_entry_id;
+      const end = question.end.document_word_entry_id;
+      const key = JSON.stringify({
+        begin,
+        end,
+      });
+
+      pieceOfTextsMap.set(key, true);
+    }
+
+    const ranges: WordRangeInput[] = [];
+
+    for (const key of pieceOfTextsMap.keys()) {
+      ranges.push({
+        begin_document_word_entry_id: JSON.parse(key).begin,
+        end_document_word_entry_id: JSON.parse(key).end,
+      });
+    }
+
+    getDocumentTextFromRange({
+      variables: {
+        ranges,
+      },
+    });
+  }, [getDocumentTextFromRange, questions]);
+
+  const pieceOfTexts = useMemo(() => {
+    if (!data || loading) {
+      return [];
+    }
+
+    return data.getDocumentTextFromRanges.list.map((item) => ({
+      pieceOfText: item.piece_of_text,
+      beginWordEntryId: item.begin_document_word_entry_id,
+      endWordEntryId: item.end_document_word_entry_id,
+    }));
+  }, [data, loading]);
 
   const handleCancel = () => {
     onClose();
@@ -33,6 +86,10 @@ export function PieceOfTextModal({
 
   return (
     <Stack gap="24px">
+      <Box style={{ textAlign: 'center' }}>
+        {loading && <CircularProgress />}
+      </Box>
+
       <Stack gap="18px">
         <Stack
           direction="row"
@@ -51,10 +108,17 @@ export function PieceOfTextModal({
       </Stack>
 
       <List sx={{ padding: 0 }}>
-        {questions.map((question) => (
-          <ListItem key={question.question_id} disablePadding>
+        {pieceOfTexts.map((item) => (
+          <ListItem key={item.pieceOfText} disablePadding>
             <ListItemButton
-              onClick={() => onSelectQuestion(question)}
+              onClick={() => {
+                onClose();
+                onSelectPiece(
+                  item.pieceOfText,
+                  item.beginWordEntryId,
+                  item.endWordEntryId,
+                );
+              }}
               sx={(theme) => ({
                 borderRadius: '10px',
                 border: `1px solid ${theme.palette.text.gray_stroke}`,
@@ -62,7 +126,7 @@ export function PieceOfTextModal({
                 padding: '16px',
               })}
             >
-              <Typography variant="body2">{question.question}</Typography>
+              <Typography variant="body2">{item.pieceOfText}</Typography>
             </ListItemButton>
           </ListItem>
         ))}
