afab72a50ba4c25917aec7a5b7590642089aabf9
Ihor_S <siafin2010@gmail.com>
2023-12-14 15:54:12 +0100

    test 5

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { graphqlUploadExpress } from 'graphql-upload-ts';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +10,11 @@ async function bootstrap() {
     origin: '*',
     allowedHeaders: '*',
   });
+
+  console.log(
+    `==============>  env  AWS_REGION ${process.env.AWS_REGION} AWS_ACCESS_KEY_ID ${process.env.AWS_ACCESS_KEY_ID}`,
+    'FileService#constructor',
+  );
   app.use(
     '/graphql',
     graphqlUploadExpress({
